a8f690bbbc692f0b51e99ccfb46df5f72bddce0b
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-11-24 22:08:20 +0100

    React geinstalleerd

---
+++ react-i-love-web/src/Message.tsx
@@ -0,0 +1,8 @@
+
+// PascalCasing
+function Message() {
+    //  JSX: Javascript XML -> babeljs.io/repl (hier kun je je code (HTML) naar javasript omzetten)
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
