15ee98505e2259b84ffd789d3663664b275f69a6
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-12-18 15:30:03 +0100

    Iets meer responsive

--- i-love-web-app/pages/index.vue
+++ i-love-web-app/pages/index.vue
@@ -18,16 +18,16 @@
     </ul>
   </nav>
   <main>
+    <!-- <div class="card-1">Ischa Gast</div> -->
     <h1>WE LOVE WEB <br />PORTFOLIO</h1>
     <h1 class="name-homepage">Maaike<br />Verplancke</h1>
   </main>
 </template>
 
-<style scoped>
-header {
-  width: max-content;
-}
+<script setup></script>
 
+<style scoped>
+/* MOBIEL */
 /* Navigation */
 nav {
   width: 100vw;
@@ -36,10 +36,10 @@ nav {
   font-family: "Unbounded", sans-serif;
 }
 
-ul {
+/* ul {
   margin: 0;
   padding-top: 2em;
-  padding-bottom: 2em;
+  padding-bottom: 1em;
   list-style: none;
   overflow: hidden;
   display: flex;
@@ -73,17 +73,18 @@ li a span:hover {
   padding: 0.5em 1em;
   border-radius: 10px;
   margin-right: 1rem;
-}
+} */
 
+/* Main */
 h1 {
   width: 100vw;
   height: 100%;
 
   color: #000;
   font-family: "Unbounded", sans-serif;
-  font-size: 8em;
+  font-size: 2em;
   font-weight: 700;
-  line-height: 149px; /* 101.361% */
+  line-height: 53px; /* 101.361% */
   text-transform: uppercase;
 }
 
@@ -91,5 +92,85 @@ h1.name-homepage {
   text-align: right;
   /* width: 100vw; */
   padding-top: 1em;
+}
+
+@media (min-width: 500px) {
+  /* Nav */
+  button.menu {
+    display: none;
+  }
+
+  /* Main */
+  h1 {
+    font-size: 5em;
+    line-height: 87px; /* 101.361% */
+  }
+}
+
+@media (min-width: 900px) {
+  /* Navigation */
+  nav {
+    width: 100vw;
+    height: auto;
+    width: 100vw;
+    font-family: "Unbounded", sans-serif;
+  }
+
+  ul {
+    margin: 0;
+    padding-top: 2em;
+    padding-bottom: 1em;
+    list-style: none;
+    overflow: hidden;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+
+  li a {
+    text-align: center;
+    padding: 1em;
+    text-decoration: none;
+    color: var(--darkblue);
+    font-size: 1rem;
+  }
+
+  li a:hover {
+    color: var(--primary-color);
+  }
+
+  li a span {
+    color: white;
+    background: var(--primary-color);
+    padding: 0.5em 1em;
+    border-radius: 10px;
+    margin-right: 1rem;
+  }
+
+  li a span:hover {
+    color: #000;
+    background: var(--secundary-purple-color);
+    padding: 0.5em 1em;
+    border-radius: 10px;
+    margin-right: 1rem;
+  }
+
+  h1 {
+    width: 100vw;
+    height: 100%;
+
+    color: #000;
+    font-family: "Unbounded", sans-serif;
+    font-size: 8em;
+    font-weight: 700;
+    line-height: 149px; /* 101.361% */
+    text-transform: uppercase;
+  }
+
+  h1.name-homepage {
+    text-align: right;
+    /* width: 100vw; */
+    padding-top: 1em;
+  }
 }
 </style>
